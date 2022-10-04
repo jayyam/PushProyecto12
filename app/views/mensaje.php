cd05efdaf81f969b73c35dfb5e84972060eaad96
omarsantana <omardpana22@gmail.com>
2022-10-04 14:19:03 +0200

    view

--- app/views/mensaje.php
+++ app/views/mensaje.php
@@ -1,7 +1,7 @@
 <?php include_once 'header.php'?>
 
 <h2 class="text-center"> <?= $data['subtitle']?> </h2>
-<div class="alert <?= $data['subtitle']?> mt-3">
+<div class="alert <?= $data['color']?> mt-3">
      <h4><?= $data['text']?></h4>
 </div>
 <a href="<?= ROOT . $data['url']?>" class="btn <?= $data['colorButton']?>">
--- app/views/olvido.php
+++ app/views/olvido.php
@@ -1,19 +1,24 @@
 <?php include_once 'header.php'?>
-
-<div class="card p-4 bg-light">
-    <div class="card-header">
-        <h1 class="text-center"><?= $data['subtitle']?></h1>
-    </div>
-    <div class="card-body">
-        <form action="<?= ROOT ?>login/olvido" method="post">
-            <div class="form-group text-left">
-                <label for="email">Correo electrónico:</label>
-                <input type="email" name="email" class="form-control">
+    <div class="card p-4 bg-light">
+        <div class="card-header">
+            <h1 class="text-center"><?= $data['subtitle'] ?></h1>
+        </div>
+        <div class="card-body">
+            <form action="<?= ROOT ?>login/olvido" method="post">
+                <div class="form-group text-left">
+                    <label for="email">Correo electrónico:</label>
+                    <input type="email" name="email" class="form-control">
+                </div>
+                <div class="form-group text-left mt-2">
+                    <input type="submit" value="Enviar" class="btn btn-success">
+                </div>
+            </form>
+        </div>
+        <div class="card-footer">
+            <div class="row">
+                <p>Recibirás un correo electrónico, comprueba la bandeja de SPAM</p>
             </div>
-            <div class="form-group text-left mt-2">
-                <input type="submit" value="Enviar" class="btn btn-success">
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
